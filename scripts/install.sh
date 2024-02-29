55f45c1c9b332282848f2fa6b8ef9bc4673cc981
Pale Kiwi <contact@palekiwi.com>
2024-02-29 16:57:15 +0800

    [dev]: add drivers, virtualization

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Installation script to be run on a fresh OS.
+
 dir=~/.venv/ansible
 
 if [ ! -d $dir ]; then
@@ -16,7 +18,11 @@ source $dir/bin/activate
 echo "Installing ansible..."
 pip install ansible
 
-echo "\nDONE\n"
+echo -e "\nDONE\n"
 
 echo "To run the main playbook:"
-echo "source ${dir}/bin/activate && ansible-playbook playbooks/main.yml -K"
+echo -e "\n"
+
+echo "source ${dir}/bin/activate"
+echo "ansible-playbook -l $(hostname -s) playbooks/main.yml -K -e 'ansible_user=$(whoami) ansible_port=22'"
+echo -e "\n"
